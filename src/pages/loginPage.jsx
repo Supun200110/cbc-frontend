8a31f1a217ba2d2ff22ff214d40a0b76b386b2ca
Supun200110 <supunnishantha45@gmail.com>
2026-02-03 12:04:50 +0530

    solving route issue for vercel😊

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -9,43 +9,43 @@ export default function LoginPage() {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const[loading,setLoading]=useState(false)
+    const [loading, setLoading] = useState(false)
     const navigate = useNavigate(); // it will navigate to the pages smoothly
-    const loginWithGoogle=useGoogleLogin(
-       {
-        onSuccess :(res)=>{
-           setLoading(true)
-           axios.post(import.meta.env.VITE_BACKEND_URL+"/api/user/google",{
-            accessToken : res.access_token
-           }).then((response)=>{
-            console.log("Login Successful", response.data);
-                toast.success("Login Successful");
-                localStorage.setItem("token", response.data.token)
+    const loginWithGoogle = useGoogleLogin(
+        {
+            onSuccess: (res) => {
+                setLoading(true)
+                axios.post(import.meta.env.VITE_BACKEND_URL + "/api/user/google", {
+                    accessToken: res.access_token
+                }).then((response) => {
+                    console.log("Login Successful", response.data);
+                    toast.success("Login Successful");
+                    localStorage.setItem("token", response.data.token)
 
-                const user = response.data.user; //taking the user data from the response
-                if (user.role == "admin") {
-                    //go to the admin page
-                  navigate("/admin");
-                }
-                else{
-                    //go to the home page
-                    navigate("/")  ;
-                }
-                setLoading(false);
-           })
+                    const user = response.data.user; //taking the user data from the response
+                    if (user.role == "admin") {
+                        //go to the admin page
+                        navigate("/admin");
+                    }
+                    else {
+                        //go to the home page
+                        navigate("/");
+                    }
+                    setLoading(false);
+                })
+            }
         }
-       }
     )
 
     function handleLogin() {
         console.log("Email", email)
         console.log("Password", password) // Saving final email and password that user entered using useState
         setLoading(true)
-        axios.post(import.meta.env.VITE_BACKEND_URL +"/api/user/login", {
+        axios.post(import.meta.env.VITE_BACKEND_URL + "/api/user/login", {
             email: email,
             password: password   //sending Email and Passsword to the backend
         }).then(
-            (response)=>{
+            (response) => {
                 console.log("Login Successful", response.data);
                 toast.success("Login Successful");
                 localStorage.setItem("token", response.data.token)
@@ -53,19 +53,23 @@ export default function LoginPage() {
                 const user = response.data.user; //taking the user data from the response
                 if (user.role == "admin") {
                     //go to the admin page
-                  navigate("/admin")
+                    navigate("/admin")
                 }
-                else{
+                else {
                     //go to the home page
-                    navigate("/")  
+                    navigate("/")
                 }
                 setLoading(false)
-            
+
             }
         ).catch(
-            (error)=>{
-                console.log("Login Failed", error.response.data) 
-                toast.error(error.response.data.message || "Login Failed")
+            (error) => {
+                console.log("Login Failed", error)
+                if (error.response && error.response.data) {
+                    toast.error(error.response.data.message || "Login Failed")
+                } else {
+                    toast.error("Server unreachable or Network Error")
+                }
                 setLoading(false)
             }
         )   //using a promise taking the response 
@@ -83,12 +87,12 @@ export default function LoginPage() {
                     <input onChange={(e) => setEmail(e.target.value)} className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px] " type="email" placeholder="Email" />
                     <input onChange={(e) => setPassword(e.target.value)} className="w-[400px] h-[50px] border border-white rounded-xl text-center " type="password" placeholder="Password" />
                     <button onClick={handleLogin} className="w-[400px] h-[50px] bg-green-500  text-white rounded-xl text-center m-[5px] cursor-pointer">
-                        {loading ? "Loading..." : "Login"}  
+                        {loading ? "Loading..." : "Login"}
                     </button>
                     <button className="w-[400px] h-[50px] bg-green-500  text-white rounded-xl text-center m-[5px] cursor-pointer flex justify-center items-center"
-                    onClick={loginWithGoogle}>
-                        
-                        <GrGoogle className="mr-[10px]"/> {loading ? "Loading..." : "Login with Google"}  
+                        onClick={loginWithGoogle}>
+
+                        <GrGoogle className="mr-[10px]" /> {loading ? "Loading..." : "Login with Google"}
 
                     </button>
                     <p className="text-gray-800 text-center m-[10px]">
